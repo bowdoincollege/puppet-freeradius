01daeff595f79c91283c02a5419dd5d35126bbad
Nathan Ward <nward@searchlight.nz>
2021-02-08 18:34:40 +1300

    Fix ruby validation errors

--- spec/unit/facter/freeradius_version_spec.rb
+++ spec/unit/facter/freeradius_version_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 require 'facter/freeradius_version'
 
-describe :freeradius_version, :type => :fact do
+describe 'freeradius_version', type: :fact do
   before :each do
     Facter.clear
-    expect(Facter::Core::Execution).to receive(:exec).with('radiusd -v').and_return('FreeRADIUS Version 3.0.21')
+    allow(Facter::Core::Execution).to receive(:exec).with('radiusd -v').and_return('FreeRADIUS Version 3.0.21')
   end
 
-  it 'should set freeradius_version' do
+  it 'sets freeradius_version' do
     expect(Facter.fact(:freeradius_version).value).to eq('3.0.21')
   end
 
-  it 'should set freeradius_maj_version' do
+  it 'sets freeradius_maj_version' do
     expect(Facter.fact(:freeradius_maj_version).value).to eq('3')
   end
 end
